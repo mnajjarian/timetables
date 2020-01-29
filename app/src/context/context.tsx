361faccc469e76e7fbc803fd747bd211cb5a15ea
Mahdi Najjarian <mnajarian@gmail.com>
2020-01-29 10:28:04 +0200

    add context api

---
+++ app/src/context/context.tsx
@@ -0,0 +1,26 @@
+import React, { createContext, useReducer, ReactNode } from 'react';
+import reducer, { initialState } from '../reducer/reducer';
+
+
+
+export const MapContext = createContext({} as any)
+
+interface Props {
+    children?: ReactNode
+}
+
+const Provider = ({ children }: Props): JSX.Element => {
+    const [data, dataDispatch] = useReducer(reducer, initialState);
+
+    const values = {
+        data,
+        dataDispatch
+    }
+    return(
+        <MapContext.Provider value={values} >
+            {children}
+        </MapContext.Provider>
+    )
+}
+
+export default Provider;
