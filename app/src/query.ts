1a63c4f80bf24bcb1b61d5ae1c26942fed384ab9
Mahdi Najjarian <mnajarian@gmail.com>
2020-01-29 10:29:55 +0200

    add query

---
+++ app/src/query.ts
@@ -0,0 +1,51 @@
+import gql from 'graphql-tag';
+
+export const FIND_PLAN = gql`
+    query findByCoord($originLat: Float!, $originLon: Float!, $destLat: Float!, $destLon: Float!) {
+        plan(from: { lat: $originLat, lon: $originLon }, to: { lat: $destLat, lon: $destLon }, numItineraries: 3) {
+            from {
+                lon
+                lat
+            }
+            to {
+                lon
+                lat
+            }
+            itineraries {
+                walkDistance
+                duration
+                startTime
+                endTime
+                legs {
+                    mode
+                    startTime
+                    endTime
+                    route {
+                        shortName
+                    }
+                    from {
+                        lat
+                        lon
+                        name
+                    }
+                    to {
+                        lat
+                        lon
+                        name
+                    }
+                    trip {
+                        gtfsId
+                        pattern {
+                            code
+                        }
+                        tripHeadsign
+                    }
+                    legGeometry {
+                        length
+                        points
+                    }
+                }
+            }
+        }
+    }
+`;
