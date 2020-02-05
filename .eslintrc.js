3aa5c7a8db004c397dda803115ac8fec6b80e234
Mahdi Najjarian <mnajarian@gmail.com>
2020-02-05 08:59:07 +0200

    add rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,54 @@
 module.exports = {
-    root: true,
-    parser: '@typescript-eslint/parser',
-    plugins: ['react', '@typescript-eslint', 'prettier'],
-    extends: [
-        'eslint:recommended',
+    "env": {
+        "commonjs": true,
+        "es6": true,
+        "node": true
+    },
+    "root": true,
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": 2018
+    },
+    "plugins": ["react", "@typescript-eslint", "prettier"],
+    "settings": {
+        "react": { "version": "latest"}
+    },
+    "extends": [
+        "eslint:recommended",
         "plugin:react/recommended",
-        'plugin:@typescript-eslint/eslint-recommended',
-        'plugin:@typescript-eslint/recommended',
-        'prettier/@typescript-eslint'
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended",
+        "prettier/@typescript-eslint"
     ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ],
+        "eqeqeq": "error",
+        "no-trailing-spaces": "error",
+        "object-curly-spacing": [
+            "error", "always"
+        ],
+        "arrow-spacing": [
+            "error", { "before": true, "after": true }
+        ],
+        "no-console": 0
+    },
 };
