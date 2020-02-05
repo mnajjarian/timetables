2ca14178518412f8406d23b594bd3300e96706b6
Mahdi Najjarian <mnajarian@gmail.com>
2020-02-05 09:59:14 +0200

    imlement e2e test with cypress

---
+++ cypress/integration/app.spec.ts
@@ -0,0 +1,18 @@
+/// <reference types="cypress" />
+
+describe('App', function() {
+  it('Search for a place and get direction should work', function() {
+    cy.visit('http://localhost:3000')
+    cy.get('input')
+      .type('Kamppi')
+    cy.get('.dropdown-item')
+      .first()
+      .click()
+    cy.get('button')
+      .click()
+      .wait(1000)
+      .get('.btn')
+      .last()
+      .click()
+  })
+})
