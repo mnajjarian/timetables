e3e2d13133ded4c590a0fffbb887786f4bd72ff6
Mahdi Najjarian <mnajarian@gmail.com>
2020-02-05 14:33:29 +0200

    test cypress

--- cypress/integration/app.spec.ts
+++ cypress/integration/app.spec.ts
@@ -2,17 +2,16 @@
 describe('App', function() {
   it('Search for a place and get direction should work', function() {
     cy.visit('http://localhost:3000')
-    cy.get('input')
+    /*     cy.get('input')
       .type('Kamppi')
     cy.get('.dropdown-item')
       .first()
       .click()
-      .wait(1000)
     cy.get('button')
       .click()
       .wait(1000)
       .get('.btn')
       .last()
-      .click()
+      .click() */
   })
 })
