f32556b0c7daa5b3a2525075db4afc07298542b1
Mahdi Najjarian <mnajarian@gmail.com>
2021-06-04 10:43:11 +0300

    add test step

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,54 +1,54 @@
 module.exports = {
-    "env": {
-        "commonjs": true,
-        "es6": true,
-        "node": true
-    },
-    "root": true,
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": 2018
-    },
-    "plugins": ["react", "@typescript-eslint", "prettier"],
-    "settings": {
-        "react": { "version": "latest"}
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/eslint-recommended",
-        "plugin:@typescript-eslint/recommended",
-        "prettier/@typescript-eslint"
+  'env': {
+    'commonjs': true,
+    'es6': true,
+    'node': true
+  },
+  'root': true,
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 2018
+  },
+  'plugins': ['react', '@typescript-eslint', 'prettier'],
+  'settings': {
+    'react': { 'version': 'latest' }
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint'
+  ],
+  'globals': {
+    'Atomics': 'readonly',
+    'SharedArrayBuffer': 'readonly'
+  },
+  'rules': {
+    'indent': [
+      'error',
+      2
     ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "rules": {
-        "indent": [
-            "error",
-            2
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "never"
-        ],
-        "eqeqeq": "error",
-        "no-trailing-spaces": "error",
-        "object-curly-spacing": [
-            "error", "always"
-        ],
-        "arrow-spacing": [
-            "error", { "before": true, "after": true }
-        ],
-        "no-console": 0
-    },
-};
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'never'
+    ],
+    'eqeqeq': 'error',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': [
+      'error', 'always'
+    ],
+    'arrow-spacing': [
+      'error', { 'before': true, 'after': true }
+    ],
+    'no-console': 0
+  },
+}
--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,15 @@
 # build environment
 FROM node:12.2.0-alpine as build
-WORKDIR /app
+WORKDIR /usr/src/app
 ENV PATH /app/node_modules/.bin:$PATH
-COPY package.json /app/package.json
+COPY package.json /
 RUN yarn install --silent
 RUN yarn add react-scripts@3.0.1 global --silent
-COPY . /app
+COPY . .
 RUN yarn build
 
 # production environment
 FROM nginx:1.16.0-alpine
-COPY --from=build /app/build /usr/share/nginx/html
+COPY --from=build /usr/src/app/build /usr/share/nginx/html
 EXPOSE 80
 CMD ["nginx", "-g", "daemon off;"]
